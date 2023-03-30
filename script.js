fc26faa913c1a49af9286a9a10acbdd8b0f20c76
Liubov Smyrnova <122973484+LiubovSmyrnova@users.noreply.github.com>
2023-03-30 10:36:34 +0200

    Update script.js

--- script.js
+++ script.js
@@ -10,22 +10,26 @@ const account = {
     listAllExpenses: function () {
         alert(`Expenses for food: ${this.food}` + `\n` + `Expenses for rent: ${this.rent}` + `\n` + `Expenses for shopping: ${this.shopping}`);
     }
+    //sumIncome: function(income,) {
+        //let sum = 0;
+        //this.income.forEach(function(amount) {
+          //sum += amount;
+        //});
+        //return sum;
+    //}
+    //getSummary: function () {
+        //alert(`Liubov, your balances are:` + `\n` + `Total balance:` + `\n` + `Total income:
+        //}}` + `\n` + `Total expenses:`)
     }
+
     
-    // an array that should hold all of your expenses, can be empty from start
-// 
+// an array that should hold all of your expenses, can be empty from start
 //an array that should hold all of your income, can be empty from start
 // addExpenses: a function that vill add an expense to the expenses array
 // addIncome: a function that vill add an income to the income array
 // listAllExpenses: a function that should list all the expenses in the expenses array
 // getSummary: a function that should summarize your total balances. It should show your total
 // income, your total expenses and also calculate your current balances (income - expenses).
-//1
-//}
-
-
-
-
 
 function menu() {
     while (true) {
@@ -35,8 +39,6 @@ function menu() {
         //account.getName();
         const addIncome = parseFloat(prompt("{How much was your income?"))
         account.income.push(addIncome);
-        console.log(account.income);
-
     } 
     if (choice === 2) {
         const choiceCategory = prompt("What was your expense? Choose from: food, rent, shopping");
@@ -44,8 +46,7 @@ function menu() {
         if (choiceCategory === "food") {
             let inputExpense = parseFloat(prompt("Enter an amount:"));
             account.food.push(inputExpense);
-            console.log(account.food);
-            
+
         }
         if (choiceCategory === "rent") {
             let inputExpenseRent = parseFloat(prompt("Enter an amount:"));
@@ -56,7 +57,6 @@ function menu() {
             account.shopping.push(inputExpenseShop);    
         }
     
-        //const inputExpense = parseFloat(prompt("How much was your expense?"));
         //choose category
         //Add expense
     }
@@ -65,6 +65,7 @@ function menu() {
         // List all expenses
     }
     if (choice === 4) {
+        account.getSummary ();
         // Get summary
     }
     //else {
